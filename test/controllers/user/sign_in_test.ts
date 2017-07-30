03031e4af42170c58c77e6345da150a33e069d54
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-30 19:23:00 +0700

    test sign in route

--- test/controllers/user/sign_in_test.ts
+++ test/controllers/user/sign_in_test.ts
@@ -1,15 +1,21 @@
 import * as assert from 'assert';
-require('../../test_helper_normal');
 import User from '../../../src/models/user';
 import app from '../../../src/index';
 import * as request from 'supertest';
 import 'mocha';
 
 describe.only('Test sign in static function', () => {
+
+    beforeEach(async () => {
+        // Create a new person
+        const pho = new User({ email: 'pho@gmail.com', password: '123' });
+        await pho.save();
+    });
+
     it('User cannot sign in with wrong info', async() => {
         const response = await request(app)
         .post('/api/user/signin')
-        .send({ email: 'aaa', password: '123' });
+        .send({ email: 'pho@gmail', password: '123' });
         assert(response.status === 400);
     });
 
@@ -21,4 +27,5 @@ describe.only('Test sign in static function', () => {
         .send({ email, password })
         assert(response.status === 200);
     });
+    
 });
